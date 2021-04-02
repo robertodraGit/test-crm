35bbcbc30e8d732c95d6e0abfae4572d9684579a
Roberto <rdrago.uni@gmail.com>
2021-04-02 16:59:14 +0200

    IndexOrd rdy. ToDo: rest of crud. End work today

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -6,6 +6,7 @@
     <a href="{{ route('orders.create') }}" class="btn btn-primary btn-block">+ New Order</a>
   </div>
 </div>
+
 <br>
 
 <div class="row">
@@ -22,39 +23,38 @@
             <th scope="col" colspan="2" class="text-center">Actions</th>
           </tr>
         </thead>
-        @if ($orders > 0)
-          <tbody>
-            
-                @foreach ($orders as $order)
-                    <tr>
-                    <th scope="row">{{ $order->id }}</th>
-                    <td>{{ $order->title }}</td>
-                    <td>{{ $order->description }}</td>
-                    <td>{{ $order->cost }}</td>
-                    <td>{{ $order->created_at }}</td>
-                    <td><a href="{{ route('orders.edit', $order) }}">[Edit]</a></td>
-                    <td><a href="#" onclick="event.preventDefault(); document.getElementById('delete-order-{{ $order->id }}-form').submit();">[Delete]</a></td>
+        @if (empty($orders))
+            <tbody>
+                  @foreach ($orders as $order)
+                      <tr>
+                      <th scope="row">{{ $order->id }}</th>
+                      <td>{{ $order->title }}</td>
+                      <td>{{ $order->description }}</td>
+                      <td>{{ $order->cost }}</td>
+                      <td>{{ $order->created_at }}</td>
+                      <td><a href="{{ route('orders.edit', $order) }}">[Edit]</a></td>
+                      <td><a href="#" onclick="event.preventDefault(); document.getElementById('delete-order-{{ $order->id }}-form').submit();">[Delete]</a></td>
 
-                    <form id="delete-order-{{ $order->id }}-form" action="{{ route('orders-delete', $order -> id) }}" method="GET" style="display: none;">
-                        @method('GET')
-                        @csrf
-                    </form>
-                    </tr>
-                @endforeach
-          </tbody>
+                      <form id="delete-order-{{ $order->id }}-form" action="{{ route('orders-delete', $order -> id) }}" method="GET" style="display: none;">
+                          @method('GET')
+                          @csrf
+                      </form>
+                      </tr>
+                  @endforeach
+            </tbody>
                 
-            @else
-
-                <tr>
-                    <th scope="row">No orders yet.</th>
-                </tr>
-                
-            @endif
+        @else
+            <tbody>
+              <tr>
+                <th scope="row">No orders yet.</th>
+              </tr>
+            </tbody>                
+        @endif
       </table>
     </div>
 </div>
 
-@if ($orders > 0)
+@if (empty($orders))
     <div class="row">
         <div class="col-md-12">
         {{ $orders->links() }}
